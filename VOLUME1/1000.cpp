9e0a97848b6e3fadb23f24073e14a20b65a7e1a8
Eklasur Rahman <mohin73k@gmail.com>
2025-02-11 15:52:26 +0000

    1000. A+B Problem

---
+++ VOLUME1/1000.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    int a, b;
+    cin >> a >> b;
+
+    int sum = a + b;
+    cout << sum << '\n';
+
+    return 0;
+}
