87f8fce7c6817b6c9701263472b691aa10fa059e
Eklas Mohin <mohin73k@gmail.com>
2025-11-01 00:42:28 +0600

    Timus 1785. Lost in Localization

--- 1785.cpp
+++ 1785.cpp
@@ -1,31 +1,19 @@
-#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
 
 int main() {
-    std::ios::sync_with_stdio(false);
-    std::cin.tie(nullptr);
-
-    int n{};
-    std::cin >> n;
-
-    if (n > 0 && n < 5) {
-        std::cout << "few\n";
-    } else if (n > 4 && n < 10) {
-        std::cout << "several\n";
-    } else if (n > 9 && n < 20) {
-        std::cout << "pack\n";    
-    } else if (n > 19 && n < 50) {
-        std::cout << "lots\n";
-    } else if (n > 49 && n < 100) {
-        std::cout << "horde\n";
-    } else if (n > 99 && n < 250) {
-        std::cout << "throng\n";
-    } else if (n > 249 && n < 500) {
-        std::cout << "swarm\n";
-    } else if (n > 499 && n < 1000) {
-        std::cout << "zounds\n";
-    } else if (n > 999) {
-        std::cout << "legion\n";
-    }
- 
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n;
+    cin >> n;
+    if (n > 0 && n < 5) cout << "few";
+    else if (n > 4 && n < 10) cout << "several";
+    else if (n > 9 && n < 20) cout << "pack";
+    else if (n > 19 && n < 50) cout << "lots";
+    else if (n > 49 && n < 100) cout << "horde";
+    else if (n > 99 && n < 250) cout << "throng";
+    else if (n > 249 && n < 500) cout << "swarm";
+    else if (n > 499 && n < 1000) cout << "zounds";
+    else if (n > 999) cout << "legion";
     return 0;
 }
