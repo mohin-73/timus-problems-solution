d60d17fd1df923547f30dc20641286f8557c0668
Eklas Mohin <mohin73k@gmail.com>
2025-10-31 22:32:50 +0600

    Timus 1877. Bicycle Codes

--- 1877.cpp
+++ 1877.cpp
@@ -1,17 +1,16 @@
-#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
 
 int main() {
-    std::ios::sync_with_stdio(false);
-    std::cin.tie(nullptr);
-
-    int first{}, second{};
-    std::cin >> first >> second;
-
-    if (first % 2 == 0 || second % 2 == 1) {
-        std::cout << "yes\n";
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int first, second;
+    cin >> first >> second;
+    if (!(first % 2) || (second % 2)) {
+        cout << "yes\n";
     } else {
-        std::cout << "no\n";
+        cout << "no\n";
     }
- 
     return 0;
 }
