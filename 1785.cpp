9812abbe0c78a21d1130a016a5cccc10a54b184a
Eklas Mohin <mohin73k@gmail.com>
2025-11-01 00:44:31 +0600

    Timus 1785. Lost in Localization

--- 1785.cpp
+++ 1785.cpp
@@ -1,4 +1,5 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
 int main() {
@@ -6,14 +7,14 @@ int main() {
     cin.tie(nullptr);
     int n;
     cin >> n;
-    if (n > 0 && n < 5) cout << "few";
-    else if (n > 4 && n < 10) cout << "several";
-    else if (n > 9 && n < 20) cout << "pack";
-    else if (n > 19 && n < 50) cout << "lots";
-    else if (n > 49 && n < 100) cout << "horde";
-    else if (n > 99 && n < 250) cout << "throng";
-    else if (n > 249 && n < 500) cout << "swarm";
-    else if (n > 499 && n < 1000) cout << "zounds";
-    else if (n > 999) cout << "legion";
+    if (n > 0 && n < 5) cout << "few\n";
+    else if (n > 4 && n < 10) cout << "several\n";
+    else if (n > 9 && n < 20) cout << "pack\n";
+    else if (n > 19 && n < 50) cout << "lots\n";
+    else if (n > 49 && n < 100) cout << "horde\n";
+    else if (n > 99 && n < 250) cout << "throng\n";
+    else if (n > 249 && n < 500) cout << "swarm\n";
+    else if (n > 499 && n < 1000) cout << "zounds\n";
+    else if (n > 999) cout << "legion\n";
     return 0;
 }
